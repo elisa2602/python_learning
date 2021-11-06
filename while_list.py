13661d8eba37834d108abb3e544a891a8ef23166
elisa2602 <elisa.neacsu.26@gmail.com>
2021-11-07 00:15:36 +0100

    removed commented code

--- while_list.py
+++ while_list.py
@@ -2,11 +2,6 @@ old_list =[1,2,3]
 new_list = []
 count = 0
 
-'''for element in old_list:
-    temp = element + 1
-    new_list.append(temp)'''
-
-# while the number of elements<3 : add 1 to every element
 while count<len(old_list):
     temp = old_list[count] + 1
     new_list.append(temp)
