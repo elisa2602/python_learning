bf270121a310204367685ceb07a26d9cd632e69f
elisa2602 <elisa.neacsu.26@gmail.com>
2021-11-07 01:12:12 +0100

    2 for

---
+++ square_lists.py
@@ -0,0 +1,14 @@
+numbers = [1,2,3,4,5,6]
+new_list = []
+new_list2 = []
+
+for n in numbers:
+    temp = n**2
+    new_list.append(temp)
+print(new_list)
+
+for m in new_list:
+    temp2 = m**2
+    new_list2.append(temp2)
+print(new_list2)
+
